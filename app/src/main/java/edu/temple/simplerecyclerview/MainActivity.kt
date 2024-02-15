3d5653c87ee7e37f895d0b67ea436049ea3cac9d
Eric Lin <tur99299@temple.edu>
2024-02-15 12:22:32 -0500

    First, we need to initialize the callback and numbers. And then we need to create a inner class with layout: view, and create a textView,
    later, we need to create a init, which setOnClickListener in it. and apply the position in it.
    
    and in main activity, we need to define a data class "NumberSize", later on, modify the callback and use lambda.
    
    finally, in textview layout, we need to change the layout height in to wrap_content, so we can see the gap between each number.

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -18,9 +18,13 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = GridLayoutManager(this, 4)
 
         // TODO (Step 2: Define lambda to modify displayTextView size)
+        data class NumberSize(val value: Float)
 
+        val callback: (Float) -> Unit = { size ->
+            displayTextView.textSize = size
+        }
         // Todo (Step 3: Pass lambda to adapter)
-        recyclerView.adapter = NumberDisplayAdapter(numbers)
+        recyclerView.adapter = NumberDisplayAdapter(numbers, callback)
 
 
     }
