0493f1bd15748eb0ff74d42aec54b034d47cbe7f
Eric Lin <tur99299@temple.edu>
2024-02-15 12:22:43 -0500

    First, we need to initialize the callback and numbers. And then we need to create a inner class with layout: view, and create a textView,
    later, we need to create a init, which setOnClickListener in it. and apply the position in it.
    
    and in main activity, we need to define a data class "NumberSize", later on, modify the callback and use lambda.
    
    finally, in textview layout, we need to change the layout height in to wrap_content, so we can see the gap between each number.

--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -9,14 +9,26 @@ import androidx.recyclerview.widget.RecyclerView
 // TODO(Step 1: Implement click behavior)
 
 // TODO (Step 1a: Accept lambda in constructor)
-class NumberDisplayAdapter (private val numbers: Array<Int>) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
-
+class NumberDisplayAdapter(private val _numbers: Array<Int>, private val _callback: (Float) -> Unit) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
+    private val callback = _callback
+    private val numbers = _numbers
     // TODO (Step 1b: Invoke lambda via onClickListener)
     inner class NumberViewHolder (layout: View) : RecyclerView.ViewHolder (layout) {
         // enumerate views inside layout
         val textView = layout.findViewById<TextView>(R.id.textView)
 
-    }
+        init {
+            layout.setOnClickListener{
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION)
+                callback.invoke(numbers[adapterPosition].toFloat())
+
+                }
+            }
+        }
+
+
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
 
